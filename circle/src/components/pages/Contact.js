389d69ded12333f4705924c8769b4c688d05e390
CanAvsar <can.avsar@ug.bilkent.edu.tr>
2021-12-21 20:13:13 +0300

    HTML & CSS for creating page drafts

--- circle/src/components/pages/Contact.js
+++ circle/src/components/pages/Contact.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import '../../App.css';
+import './PageStyles.css';
+
 
 // homepage
 export default function Contact() {
     return(
-        <>  
-        <h1 className='contact'>Contact</h1>
-        </>
+    <> 
+    <div class="titlebar">
+        <li>
+            <h1 className='Contact'>Contact</h1>
+        </li>
+    </div>
+        
+    <h2 className='ContactWithCLub'>Contact with Your Clubs</h2>
+    <hr />
+
+    <h2 className='ContactWithDevelopers'>Contact with Developers</h2>
+    <hr />
+
+    </>
     );
 }
--- circle/src/components/pages/Events.js
+++ circle/src/components/pages/Events.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import '../../App.css';
+import './PageStyles.css';
+import Cards from '../Cards';
 
-// homepage
+
+// Events
 export default function Events() {
     return(
-        <>  
-        <h1 className='events'>EVENTS</h1>
-        </>
+    <> 
+    <div class="titlebar">
+    <li>
+        <h1>Events</h1>
+    </li>
+        
+        <div class="search">
+            <li><input type="text" placeholder="Search..."/></li>
+            <li><button class="filterButton"> Filter </button></li>
+        </div>
+    </div>
+
+    <h2>This Week</h2>
+    <hr />
+    <Cards/>
+
+    <h2 id="secondTitle">Next Week</h2>
+    <hr />
+    <Cards/>
+
+    </>
     );
 }
