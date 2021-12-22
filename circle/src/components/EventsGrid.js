24427cf44228b19fcaae1bb464a1aa02cdad1f1c
Ayda Yurtoglu <aydayurtoglu@gmail.com>
2021-12-22 10:38:49 +0300

    Update EventsGrid.js

--- circle/src/components/EventsGrid.js
+++ circle/src/components/EventsGrid.js
@@ -6,25 +6,25 @@ import Event  from './EventComponent.js';
 import Button from '@material-ui/core/Button';
 
 const EventsGrid = (props) => {
-  const handleClick = (value) => {
-  
+    const handleClick = (value) => {
+    
     }
 
-  return (
-    <div>
-      <Grid spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justifyContent="left" spacing={2}>
-            {props.items.map((value) => (
-              <Grid key={value.slug} item>
-                <Event name={value.name} price={value.price}> <p>TL</p></Event>
-                <Button itemSlug={value.slug} onChange={(e) => handleClick(e.target.value)} variant="contained" color="primary">Add</Button>
-              </Grid>
-            ))}
-          </Grid>
+    return (
+        <div>
+        <Grid spacing={2}>
+            <Grid item xs={12}>
+            <Grid container justifyContent="left" spacing={2}>
+                {props.items.map((value) => (
+                <Grid key={value.slug} item>
+                    <Event name={value.name} price={value.price}> <p>TL</p></Event>
+                    <Button itemSlug={value.slug} onChange={(e) => handleClick(e.target.value)} variant="contained" color="primary">Add</Button>
+                </Grid>
+                ))}
+            </Grid>
+            </Grid>
         </Grid>
-      </Grid>
-    </div>
-  );
+        </div>
+    );
 }
 export default EventsGrid;
