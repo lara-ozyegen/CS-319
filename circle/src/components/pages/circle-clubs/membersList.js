c6af204a4bc6259cf0589dbff4bb04ddb094bfe6
Ayda Yurtoglu <aydayurtoglu@gmail.com>
2021-12-25 21:14:02 +0300

    list

---
+++ circle/src/components/pages/circle-clubs/membersList.js
@@ -0,0 +1,81 @@
+const items = [
+    {
+        "name": "Ayda",
+        "surname": "Yurtoğlu",
+        "email": "ayda.yurtoglu@ug.bilkent.edu.tr",
+        "position": "member"
+    },
+    {
+        "name": "Ata",
+        "surname": "Seren",
+        "email": "ata.seren@ug.bilkent.edu.tr",
+        "position": "active member"
+    },
+    {
+        "name": "Can",
+        "surname": "Avsar",
+        "email": "can.avsar@ug.bilkent.edu.tr",
+        "position": "member"
+    },
+    {
+        "name": "Sarper",
+        "surname": "Turan",
+        "email": "sarper.turan@ug.bilkent.edu.tr",
+        "position": "member"
+    },
+    {
+        "name": "Lara",
+        "surname": "Ozyegen",
+        "email": "lara.ozyegen@ug.bilkent.edu.tr",
+        "position": "board member"
+    },
+    {
+        "name": "lorem",
+        "surname": "ipsum",
+        "email": "lorem.ipsum@ug.bilkent.edu.tr",
+        "position": "board member"
+    },
+    {
+        "name": "lorem",
+        "surname": "ipsum",
+        "email": "lorem.ipsum@ug.bilkent.edu.tr",
+        "position": "board member"
+    },
+    {
+        "name": "lorema",
+        "surname": "ipsum",
+        "email": "lorem.ipsum@ug.bilkent.edu.tr",
+        "position": "active member"
+    },
+    {
+        "name": "lorem",
+        "surname": "ipsum",
+        "email": "lorem.ipsum@ug.bilkent.edu.tr",
+        "position": "member"
+    },
+    {
+        "name": "lorem",
+        "surname": "ipsusm",
+        "email": "lorem.ipsum@ug.bilkent.edu.tr",
+        "position": "active member"
+    },
+    {
+        "name": "lorem",
+        "surname": "ipsudm",
+        "email": "lorem.ipsum@ug.bilkent.edu.tr",
+        "position": "member"
+    },
+    {
+        "name": "lorem",
+        "surname": "ipsum",
+        "email": "lorem.ipsum@ug.bilkent.edu.tr",
+        "position": "active member"
+    },
+    {
+        "name": "lorem",
+        "surname": "ipsum",
+        "email": "loremd.ipsum@ug.bilkent.edu.tr",
+        "position": "board member"
+    },
+    ]
+    export default items;
